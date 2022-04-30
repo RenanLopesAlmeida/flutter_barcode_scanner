9992642f6a3fcb79f989295f972359b22feb8542
RenanLopesAlmeida <renan.lopes.contato@gmail.com>
2022-04-30 15:20:41 -0300

    changing barcodes file name

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:barcodes_flutter_app/modules/barcodes_list/ui/barcodes_list/screen/barcodes_list_screen.dart';
+import 'package:barcodes_flutter_app/modules/barcodes/ui/barcodes/screen/barcodes_screen.dart';
 import 'package:flutter/material.dart';
 
 void main() {
