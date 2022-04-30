c2e5eb229baba990d663216f2a12571969c1d88f
RenanLopesAlmeida <renan.lopes.contato@gmail.com>
2022-04-30 17:44:35 -0300

    moving barcodes cubit to its own file

---
+++ lib/modules/barcodes/cubits/barcodes_cubit.dart
@@ -0,0 +1,27 @@
+import 'package:barcodes_flutter_app/core/ports/input/scan_code_input_port.dart';
+import 'package:barcodes_flutter_app/core/typedefs/scan_option.dart';
+import 'package:barcodes_flutter_app/modules/barcodes/models/barcode.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+class BarcodesCubit extends Cubit<List<Barcode>> {
+  BarcodesCubit({
+    required final ScanCodeInputPort scanBarCodeInputPort,
+  })  : _scanBarCodeInputPort = scanBarCodeInputPort,
+        super([]);
+
+  final ScanCodeInputPort _scanBarCodeInputPort;
+
+  void addBarcode(Barcode barcode) {
+    state.add(barcode);
+    emit(state);
+  }
+
+  void scanBarCode(final ScanOption scanOption) {
+    _scanBarCodeInputPort.scanCode(scanOption: scanOption).listen((
+      final barcode,
+    ) {
+      state.add(barcode);
+      emit(state);
+    });
+  }
+}
--- test/modules/barcodes/cubit/barcodes_cubit_test.dart
+++ test/modules/barcodes/cubit/barcodes_cubit_test.dart
@@ -1,35 +1,12 @@
 import 'package:barcodes_flutter_app/core/ports/input/scan_code_input_port.dart';
 import 'package:barcodes_flutter_app/core/typedefs/scan_option.dart';
+import 'package:barcodes_flutter_app/modules/barcodes/cubits/barcodes_cubit.dart';
 import 'package:barcodes_flutter_app/modules/barcodes/models/barcode.dart';
 import 'package:bloc_test/bloc_test.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mocktail/mocktail.dart';
 
 import '../../../mocks/mock_bar_code_scanner_input_port.dart';
-
-class BarcodesCubit extends Cubit<List<Barcode>> {
-  BarcodesCubit({
-    required final ScanCodeInputPort scanBarCodeInputPort,
-  })  : _scanBarCodeInputPort = scanBarCodeInputPort,
-        super([]);
-
-  final ScanCodeInputPort _scanBarCodeInputPort;
-
-  void addBarcode(Barcode barcode) {
-    state.add(barcode);
-    emit(state);
-  }
-
-  void scanBarCode(final ScanOption scanOption) {
-    _scanBarCodeInputPort.scanCode(scanOption: scanOption).listen((
-      final barcode,
-    ) {
-      state.add(barcode);
-      emit(state);
-    });
-  }
-}
 
 class MockScanCodeInputPort extends Mock implements ScanCodeInputPort {}
 
