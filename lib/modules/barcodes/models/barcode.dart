6f3b20c3b5fbc411f911b7f93bddec0b197b40fa
RenanLopesAlmeida <renan.lopes.contato@gmail.com>
2022-04-30 22:00:13 -0300

    making private barcode property

--- lib/modules/barcodes/models/barcode.dart
+++ lib/modules/barcodes/models/barcode.dart
@@ -1,9 +1,14 @@
 import 'package:equatable/equatable.dart';
 
 class Barcode extends Equatable {
-  const Barcode({required final this.content});
-  final String content;
+  const Barcode({
+    required final String content,
+  }) : _content = content;
+
+  final String _content;
+
+  String get content => _content;
 
   @override
-  List<Object?> get props => [content];
+  List<Object?> get props => [_content];
 }
