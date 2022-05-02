91cd822c8de4160c7fd9af0cbe0c4a3b161e570c
RenanLopesAlmeida <renan.lopes.contato@gmail.com>
2022-05-02 10:12:50 -0300

    removing unnecessary code

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,6 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:barcodes_flutter_app/core/service_locator/service_locator_config.dart';
 import 'package:barcodes_flutter_app/modules/barcodes/cubits/barcode_picker_cubit.dart';
 import 'package:barcodes_flutter_app/modules/barcodes/cubits/barcode_state_cubit.dart';
-import 'package:barcodes_flutter_app/utils/app_redirect_launcher.dart';
 import 'package:barcodes_flutter_app/modules/barcodes/cubits/barcodes_cubit.dart';
 import 'package:barcodes_flutter_app/modules/barcodes/ui/barcodes/screen/barcodes_screen.dart';
 
@@ -37,12 +36,10 @@ class MyApp extends StatelessWidget {
             value: BarcodeStateCubit(),
             child: const BarcodesListScreen(),
           ),
-
           BlocProvider<BarcodePickerCubit>.value(
             value: BarcodePickerCubit(),
             child: const BarcodesListScreen(),
           ),
-          //BarcodePicker
         ],
         child: const BarcodesListScreen(),
       ),
